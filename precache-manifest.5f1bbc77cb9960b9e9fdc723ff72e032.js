2406a9855f3b8224080c226f01ce5cddb8cddc32
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-04-12 16:45:09 +0530

    Updates

---
+++ precache-manifest.5f1bbc77cb9960b9e9fdc723ff72e032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6552231910656d71560dbd9ed9803d",
+    "url": "/TODOList/index.html"
+  },
+  {
+    "revision": "86bd348e8235d6eb1e7b",
+    "url": "/TODOList/static/css/main.63e6fd13.chunk.css"
+  },
+  {
+    "revision": "6d673b61a729cf6aba4a",
+    "url": "/TODOList/static/js/2.5cd9e1e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TODOList/static/js/2.5cd9e1e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86bd348e8235d6eb1e7b",
+    "url": "/TODOList/static/js/main.e8185dcc.chunk.js"
+  },
+  {
+    "revision": "a94ce8ec6dd92c5405f8",
+    "url": "/TODOList/static/js/runtime-main.04bd82b2.js"
+  }
+]);
